ea646ef6a8b78ef42a17dea17d984aacf1511906
Unknown <jdeansart@gmail.com>
2017-10-22 22:42:06 -0500

    2 push

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// 1part
+
 $(document).ready(function(){
     var data = {
         cost: 9.99
@@ -19,4 +21,35 @@ $(document).ready(function(){
     $('#unit-price').html('$' + data.cost + ' ea.')
     addAttendee()
     syncPurchaseButton()
+})
+
+// 2part
+
+function addAttendee(){
+    $('.attendee-list').append(
+        $('script[data-template="attendee"').text()
+    )
+    syncRemoveButtons()
+}
+function syncRemoveButtons(){
+    if (getAttendeeCount() === 1){
+        $('.attendee-list .attendee .remove-attendee').first().hide()
+    }
+    else {
+        $('.attendee-list .attendee .remove-attendee').show()
+    }
+}
+function syncPurchaseButton(){
+    $('#checkout-button span.amount').html(
+        '$' + data.cost * getAttendeeCount()
+    )
+}
+$('add-attendee').on('click', function (event){
+    event.preventDefault()
+    addAttendee()
+    $(this).trigger('attendee:add')
+
+}).on('attendee:add', function(){
+    syncPurchaseButton()
+    syncRemoveButtons()
 })
