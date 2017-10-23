8f2419015c2ca08179e99f9fe22a5caee94c8841
Unknown <jdeansart@gmail.com>
2017-10-23 13:12:20 -0500

    fix

--- app.js
+++ app.js
@@ -1,69 +1,58 @@
-// 1part
+$(document).ready(function () {
+    
+        var data = {
+            cost: 9.99
+        };
 
-$(document).ready(function(){
-    var data = {
-        cost: 9.99
-    };
-    function getAttendeeCount(){
-        return $('.attendee-list .row.attendee').length
-
-    }
-    function addAttendee(){
-        $('.attendee-list').append(
-            $('script[data-template="attendee"]').text()
-        )
-    }
-    function syncPurchaseButton(){
-        $('#checkout-button span.amount').html(
-            '$' + data.cost * getAttendeeCount()
-        )
-    }
-    $('#unit-price').html('$' + data.cost + ' ea.')
-    addAttendee()
-    syncPurchaseButton()
-
-
-// 2part
-
-    function addAttendee(){
-        $('.attendee-list').append(
-            $('script[data-template="attendee"').text()
-        )
-        syncRemoveButtons()
-    }
-    function syncRemoveButtons(){
-        if (getAttendeeCount() === 1){
-            $('.attendee-list .attendee .remove-attendee').first().hide()
+        function getAttendeeCount() {
+            return $('.attendee-list .row.attendee').length;
         }
-        else {
-            $('.attendee-list .attendee .remove-attendee').show()
+    
+        function addAttendee() {
+            $('.attendee-list').append(
+                $('script[data-template="attendee"]').text()
+            );
+    
+            syncRemoveButtons();
         }
-    }
-    function syncPurchaseButton(){
-        $('#checkout-button span.amount').html(
-            '$' + data.cost * getAttendeeCount()
-        )
-    }
-    $('add-attendee').on('click', function (event){
-        event.preventDefault()
-        addAttendee()
-        $(this).trigger('attendee:add')
-
-    }).on('attendee:add', function(){
-        syncPurchaseButton()
-        syncRemoveButtons()
-    })
-    $('#app').on('attendee:remove', function(){
-        event.preventDefault()
-        var $row = $(event.target).closest('.attendee.row')
-        $row.remove()
-        $('#app').trigger('attendee:remove')
-    })
-    $('#app').on('attendee:remove', function(){
-        syncPurchaseButton()
-        syncRemoveButtons()
-    })
-    $('#unit-price').html('$' + data.cost + ' ea.')
-    addAttendee()
-    syncPurchaseButton()
-})
+    
+        function syncRemoveButtons() {
+            if (getAttendeeCount() === 1) {
+                $('.attendee-list .attendee .remove-attendee').first().hide();
+            } else {
+                $('.attendee-list .attendee .remove-attendee').show();
+            }
+        }
+    
+        function syncPurchaseButton() {
+            $('#checkout-button span.amount').html(
+                '$' + data.cost * getAttendeeCount()
+            );
+        }
+    
+        $('.add-attendee').on('click', function (event) {
+            event.preventDefault();
+            addAttendee();
+            $(this).trigger('attendee:add');
+        }).on('attendee:add', function () {
+            syncPurchaseButton();
+            syncRemoveButtons();
+        });
+    
+        $('#app').on('click', '.attendee .remove-attendee', function (event) {
+            event.preventDefault();
+            var $row = $(event.target).closest('.attendee.row');
+    
+            $row.remove();
+            $('#app').trigger('attendee:remove');
+        });
+    
+        $('#app').on('attendee:remove', function () {
+            syncPurchaseButton();
+            syncRemoveButtons();
+        });
+    
+        $('#unit-price').html('$' + data.cost + ' ea.');
+        addAttendee();
+        syncPurchaseButton();
+    });
