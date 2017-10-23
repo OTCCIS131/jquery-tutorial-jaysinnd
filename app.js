0501ff1d6187d8dca4971ec1a50bf4d13e09fc25
Unknown <jdeansart@gmail.com>
2017-10-22 22:51:54 -0500

    not working correctly? followed all instructions to the tee..

--- app.js
+++ app.js
@@ -21,35 +21,49 @@ $(document).ready(function(){
     $('#unit-price').html('$' + data.cost + ' ea.')
     addAttendee()
     syncPurchaseButton()
-})
+
 
 // 2part
 
-function addAttendee(){
-    $('.attendee-list').append(
-        $('script[data-template="attendee"').text()
-    )
-    syncRemoveButtons()
-}
-function syncRemoveButtons(){
-    if (getAttendeeCount() === 1){
-        $('.attendee-list .attendee .remove-attendee').first().hide()
+    function addAttendee(){
+        $('.attendee-list').append(
+            $('script[data-template="attendee"').text()
+        )
+        syncRemoveButtons()
     }
-    else {
-        $('.attendee-list .attendee .remove-attendee').show()
+    function syncRemoveButtons(){
+        if (getAttendeeCount() === 1){
+            $('.attendee-list .attendee .remove-attendee').first().hide()
+        }
+        else {
+            $('.attendee-list .attendee .remove-attendee').show()
+        }
     }
-}
-function syncPurchaseButton(){
-    $('#checkout-button span.amount').html(
-        '$' + data.cost * getAttendeeCount()
-    )
-}
-$('add-attendee').on('click', function (event){
-    event.preventDefault()
-    addAttendee()
-    $(this).trigger('attendee:add')
+    function syncPurchaseButton(){
+        $('#checkout-button span.amount').html(
+            '$' + data.cost * getAttendeeCount()
+        )
+    }
+    $('add-attendee').on('click', function (event){
+        event.preventDefault()
+        addAttendee()
+        $(this).trigger('attendee:add')
 
-}).on('attendee:add', function(){
+    }).on('attendee:add', function(){
+        syncPurchaseButton()
+        syncRemoveButtons()
+    })
+    $('#app').on('attendee:remove', function(){
+        event.preventDefault()
+        var $row = $(event.target).closest('.attendee.row')
+        $row.remove()
+        $('#app').trigger('attendee:remove')
+    })
+    $('#app').on('attendee:remove', function(){
+        syncPurchaseButton()
+        syncRemoveButtons()
+    })
+    $('#unit-price').html('$' + data.cost + ' ea.')
+    addAttendee()
     syncPurchaseButton()
-    syncRemoveButtons()
 })
